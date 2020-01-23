11913f9e0523155e9df310df7c6308aa27f6a344
Benjamin Knudsen <benjaschou12@yahoo.dk>
2020-01-23 16:16:36 +0100

    Nyt

--- main.js
+++ main.js
@@ -2,27 +2,29 @@ var dropdown = document.getElementsByClassName("dropdown-btn");
 var i;
 
 for (i = 0; i < dropdown.length; i++) {
-    dropdown[i].addEventListener("click", function () {
-        this.classList.toggle("active");
-        var dropdownContent = this.nextElementSibling;
-        if (dropdownContent.style.display === "block") {
-            dropdownContent.style.display = "none";
-        } else {
-            dropdownContent.style.display = "block";
-        }
-    });
+  dropdown[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var dropdownContent = this.nextElementSibling;
+    if (dropdownContent.style.display === "block") {
+      dropdownContent.style.display = "none";
+    } else {
+      dropdownContent.style.display = "block";
+
+    }
+  });
 }
 
-$(document).ready(function(){
-	$('#drawer-toggle').click(function(){
-		$('#nav-icon1').toggleClass('open');
-	});
+
+$(document).ready(function () {
+  $('#drawer-toggle').click(function () {
+    $('#nav-icon1').toggleClass('open');
+  });
 });
 
-$(document).ready(function(){
-	$('#drawer-togglem').click(function(){
-		$('#nav-icon1m').toggleClass('open');
-	});
+$(document).ready(function () {
+  $('#drawer-togglem').click(function () {
+    $('#nav-icon1m').toggleClass('open');
+  });
 });
 
 
@@ -31,65 +33,64 @@ var dropdown1 = document.getElementsByClassName("dropdown-btn-m");
 var i;
 
 for (i = 0; i < dropdown1.length; i++) {
-    dropdown1[i].addEventListener("click", function () {
-        this.classList.toggle("active");
-        var dropdownContent = this.nextElementSibling;
-        if (dropdownContent.style.display === "block") {
-            dropdownContent.style.display = "none";
-        } else {
-            dropdownContent.style.display = "block";
-        }
-    });
+  dropdown1[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var dropdownContent = this.nextElementSibling;
+    if (dropdownContent.style.display === "block") {
+      dropdownContent.style.display = "none";
+    } else {
+      dropdownContent.style.display = "block";
+    }
+  });
 }
 
 
 $(".rotate").click(function () {
-    $(this).toggleClass("down");
+  $(this).toggleClass("down");
 })
 
 
 
-$(document).ready(function(){
-	$('.dropdown-btn').click(function(){
-		$('.dropdown-container').toggleClass('up');
-	});
+$(document).ready(function () {
+  $('.dropdown-btn').click(function () {
+    $('.dropdown-container').toggleClass('up');
+  });
 });
 
 
 
 $('.responsive').slick({
-    dots: false,
-    infinite: true,
-    prevArrow: '<button class="slide-arrow prev-arrow"></button>',
-    nextArrow: '<button class="slide-arrow next-arrow"></button>',
-    speed: 300,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    responsive: [
-      {
-        breakpoint: 1224,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          infinite: true,
-          centerMode: false
-          
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
+  dots: false,
+  infinite: true,
+  prevArrow: '<button class="slide-arrow prev-arrow"></button>',
+  nextArrow: '<button class="slide-arrow next-arrow"></button>',
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  responsive: [{
+      breakpoint: 1224,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        centerMode: false
+
       }
-     
-    ]
-  });
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+
+  ]
+});
