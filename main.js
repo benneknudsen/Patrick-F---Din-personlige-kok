989c6578ea32a80133ff6b743a33c9b95015c470
Benjamin Knudsen <benjaschou12@yahoo.dk>
2020-01-23 14:12:35 +0100

    JS FIX

--- main.js
+++ main.js
@@ -26,20 +26,6 @@ $(document).ready(function(){
 });
 
 
-var dropdown2 = document.getElementsByClassName("#drawer ul a");
-var i;
-
-for (i = 0; i < dropdown2.length; i++) {
-    dropdown2[i].addEventListener("click", function () {
-        this.classList.toggle("active");
-        var dropdownContent = this.nextElementSibling;
-        if (dropdownContent.style.display === "block") {
-            dropdownContent.style.display = "none";
-        } else {
-            dropdownContent.style.display = "block";
-        }
-    });
-}
 
 const btn = document.getElementById('elem');
 
@@ -116,4 +102,3 @@ $('.responsive').slick({
      
     ]
   });
-     
